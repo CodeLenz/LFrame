4b819c4c4408c5b78772d77adce32855a43162b7
Eduardo Lenz <eduardobarplenz@gmail.com>
2025-05-14 15:35:29 -0300

    Mais testes de consistência do YAML

--- test/test_yaml.jl
+++ test/test_yaml.jl
@@ -19,5 +19,25 @@
     # No material 
     @test_throws ErrorException LFrame.Le_YAML("data/yaml/yaml_no_materiais.yaml")
 
-    
+    ##############################################
+    # No coordenadas
+    @test_throws ErrorException LFrame.Le_YAML("data/yaml/yaml_no_coordenadas.yaml")
+
+    ##############################################
+    # No apoios
+    @test_throws ErrorException LFrame.Le_YAML("data/yaml/yaml_no_apoios.yaml")
+
+    ##############################################
+    # No geometrias
+    @test_throws ErrorException LFrame.Le_YAML("data/yaml/yaml_no_geometrias.yaml")
+
+    ##############################################
+    # No dados_elementos
+    @test_throws ErrorException LFrame.Le_YAML("data/yaml/yaml_no_dados_elementos.yaml")
+
+    ##############################################
+    # Informa material que não existe em dados_elementos
+    @test_throws ErrorException LFrame.Le_YAML("data/yaml/yaml_consist_dados_elementos.yaml")
+
+
 end
